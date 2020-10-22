3bd815f1682948187934dfb3c0b3450fc25cdf9b
Francois-Xavier <francoisx.cyr@gmail.com>
2020-10-21 21:16:12 -0400

    75% done

--- public/cat/scripts.js
+++ public/cat/scripts.js
@@ -1,1 +1,38 @@
-// here there be JS, yarrr ☠️
+const messageInput = document.querySelector("#user-input");
+const conversationElem = document.querySelector("#conversation-container");
+
+handleFocus = () => {
+  messageInput.focus();
+};
+const updateConversation = (message) => {
+  // deconstruct the message object
+  const { author, text } = message;
+  // create a <p> element
+  const messageElem = document.createElement("p");
+  // add the text message to the element
+  messageElem.innerHTML = `<span>${text}</span>`;
+  messageElem.classList.add("message", author);
+  // append the element to the conversation
+  conversationElem.appendChild(messageElem);
+  conversationElem.scrollTop = conversationElem.scrollHeight;
+  console.log(message);
+  if (author === "user") {
+    messageInput.value = "";
+  }
+};
+
+const sendMessage = (event) => {
+  // prevent the default "page reload" from occurring.
+  const message = { author: "user", text: messageInput.value };
+  event.preventDefault();
+  updateConversation(message);
+
+  fetch("/cat-message")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.message);
+      updateConversation(data.message);
+    });
+};
+
+handleFocus();
--- server.js
+++ server.js
@@ -1,29 +1,92 @@
-'use strict';
+"use strict";
 
+const { query } = require("express");
 // import the needed node_modules.
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 
 express()
   // Below are methods that are included in express(). We chain them for convenience.
   // --------------------------------------------------------------------------------
 
   // This will give us will log more info to the console. see https://www.npmjs.com/package/morgan
-  .use(morgan('tiny'))
+  .use(morgan("tiny"))
 
   // Any requests for static files will go into the public folder
-  .use(express.static('public'))
+  .use(express.static("public"))
 
-  // Nothing to modify above this line
-  // ---------------------------------
-  // add new endpoints here 👇
+  // Cat endpoints here 👇
+  .get("/cat-message", (req, res) => {
+    console.log("This is cat");
+    const message = { author: "cat", text: "Meow" };
+    const randomTime = Math.floor(Math.random() * 3000);
+    setTimeout(() => {
+      res.status(200).json({ status: 200, message });
+    }, randomTime);
+  })
 
-  // add new endpoints here ☝️
-  // ---------------------------------
-  // Nothing to modify below this line
+  // Monkey endpoints here ☝️
+  .get("/monkey-message", (req, res) => {
+    const messages = [
+      "Don’t monkey around with me.",
+      "If you pay peanuts, you get monkeys.",
+      "I fling 💩 at you!",
+      "🙊",
+      "🙈",
+      "🙉",
+    ];
+    const theMessage = messages[Math.floor(Math.random() * messages.length)];
+    const message = { author: "monkey", text: theMessage };
+    const randomTime = Math.floor(Math.random() * 3000);
+    setTimeout(() => {
+      res.status(200).json({ status: 200, message });
+    }, randomTime);
+  })
+
+  // Parrot endpoints here 👇
+  .get("/parrot-message", (req, res) => {
+    console.log(req.query);
+    const userText = req.query.text;
+    const message = { author: "parrot", text: userText };
+    const randomTime = Math.floor(Math.random() * 3000);
+    setTimeout(() => {
+      res.status(200).json({ status: 200, message });
+    }, randomTime);
+  })
+
+  // Bot endpoints here 👇
+  .get("/bot-message", (req, res) => {
+    const getBotMessage = (text) => {
+      const commonGreetings = ["hi", "hello", "howdy"];
+      const commonGoodbyes = [
+        "goodbye",
+        "bye",
+        "see you",
+        "thanks",
+        "thank you",
+      ];
+      const lowerCasedText = text.toLowerCase();
+
+      let botMsg = "";
+      if (commonGreetings.includes(lowerCasedText)) {
+        botMsg = `Bzzt Hello, this is bot 🤖`;
+      } else if (commonGoodbyes.includes(lowerCasedText)) {
+        botMsg = `Bzzt Goodbye, human`;
+      } else {
+        botMsg = `Bzzt ${text}`;
+      }
+      return botMsg;
+    };
+    const userText = req.query.text;
+    const message = { author: "bot", text: getBotMessage(userText) };
+    const randomTime = Math.floor(Math.random() * 2000);
+    setTimeout(() => {
+      res.status(200).json({ status: 200, message });
+    }, randomTime);
+  })
 
   // this serves up the homepage
-  .get('/', (req, res) => {
+  .get("/", (req, res) => {
     res
       .status(200)
       .json({ status: 200, message: "This is the homepage... it's empty :(" });
@@ -31,13 +94,11 @@ express()
 
   // this is our catch all endpoint. If a user navigates to any endpoint that is not
   // defined above, they get to see our 404 page.
-  .get('*', (req, res) => {
-    res
-      .status(404)
-      .json({
-        status: 404,
-        message: 'This is obviously not the page you are looking for.',
-      });
+  .get("*", (req, res) => {
+    res.status(404).json({
+      status: 404,
+      message: "This is obviously not the page you are looking for.",
+    });
   })
 
   // Node spins up our server and sets it to listen on port 8000.
