53fd95e9b95a4589e17fa196f3b5ec25cf6c7592
Francois-Xavier <francoisx.cyr@gmail.com>
2020-10-22 13:26:16 -0400

    Workshop completed

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const { query } = require("express");
 // import the needed node_modules.
 const express = require("express");
 const morgan = require("morgan");
+let jokeApproval = false;
 
 express()
   // Below are methods that are included in express(). We chain them for convenience.
@@ -58,6 +59,7 @@ express()
   .get("/bot-message", (req, res) => {
     const getBotMessage = (text) => {
       const commonGreetings = ["hi", "hello", "howdy"];
+      const jokeEnabler = "something funny";
       const commonGoodbyes = [
         "goodbye",
         "bye",
@@ -65,6 +67,21 @@ express()
         "thanks",
         "thank you",
       ];
+      const jokes = [
+        "I’ve been hearing news about this big boolean. Huge if true.",
+        "Why couldn't the bicycle stand up by itself? It was two tired.",
+        "How do you get a squirrel to like you? Act like a nut.",
+        "What do you call someone with no body and no nose? Nobody knows.",
+        "Dad, did you get a haircut? No, I got them all cut!",
+      ];
+
+      // const jokes = [
+      //   "I’ve been hearing news about this big boolean. Huge if true.",
+      //   "Why couldn't the bicycle stand up by itself? It was two tired.",
+      //   "How do you get a squirrel to like you? Act like a nut.",
+      //   "What do you call someone with no body and no nose? Nobody knows.",
+      //   "Dad, did you get a haircut? No, I got them all cut!",
+      // ];
       const lowerCasedText = text.toLowerCase();
 
       let botMsg = "";
@@ -72,6 +89,18 @@ express()
         botMsg = `Bzzt Hello, this is bot 🤖`;
       } else if (commonGoodbyes.includes(lowerCasedText)) {
         botMsg = `Bzzt Goodbye, human`;
+      }
+      //Exercice 5 starts here
+      else if (jokeEnabler.includes(lowerCasedText)) {
+        jokeApproval = true;
+        botMsg =
+          "I'll tell you a funny joke... only if you say Yes. If not, say no.";
+      } else if (lowerCasedText.includes("yes") && jokeApproval) {
+        botMsg = jokes[Math.floor(Math.random() * jokes.length)];
+        jokeApproval = false;
+      } else if (lowerCasedText.includes("no") && jokeApproval) {
+        botMsg = "Bouhhhhhhh👎";
+        jokeApproval = false;
       } else {
         botMsg = `Bzzt ${text}`;
       }
